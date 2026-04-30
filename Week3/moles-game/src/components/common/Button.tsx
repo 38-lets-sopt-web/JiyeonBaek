13c263d814a332717032a358450b4d0e13db767f
jyeon03 <bjlyeon2003@naver.com>
2026-04-30 22:26:13 +0900

    feat: Button 컴포넌트 구현

---
+++ Week3/moles-game/src/components/common/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+  bgColor: string;
+  textColor: string;
+}
+
+const Button = ({ onClick, children, bgColor, textColor }: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`rounded-full px-5 py-2 text-lg transition ${bgColor} ${textColor}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
