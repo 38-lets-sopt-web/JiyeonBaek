ae8e54a77e951649191b001ea6ace0dc53112210
jyeon03 <bjlyeon2003@naver.com>
2026-05-01 01:02:26 +0900

    feat: Card 공통 컴포넌트 퍼블리싱

---
+++ Week3/moles-game/src/components/common/Card.tsx
@@ -0,0 +1,41 @@
+import { cn } from '@/utils/cn';
+
+type ValueType = string | number;
+type ValueSize = 'large' | 'small';
+
+interface CardProps {
+  label: string;
+  value: ValueType;
+  labelColor?: string;
+  valueSize?: ValueSize;
+  className?: string;
+}
+
+const Card = ({
+  label,
+  value,
+  labelColor = 'text-text',
+  valueSize = 'large',
+  className,
+}: CardProps) => {
+  return (
+    <article
+      className={cn(
+        'bg-primary100',
+        'flex h-48 flex-col items-center justify-center',
+        'rounded-2xl',
+        className,
+      )}
+    >
+      <p className={cn('text-2xl font-medium', labelColor)}>{label}</p>
+
+      <strong
+        className={cn('text-text mt-4 font-bold', valueSize === 'large' ? 'text-6xl' : 'text-2xl')}
+      >
+        {value}
+      </strong>
+    </article>
+  );
+};
+
+export default Card;
