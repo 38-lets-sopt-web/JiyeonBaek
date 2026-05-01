9c50ce413810213ba472881e67713a9ea5a8c87b
jyeon03 <bjlyeon2003@naver.com>
2026-05-01 15:29:42 +0900

    feat: RankingHeader 컴포넌트 구현

---
+++ Week3/moles-game/src/constants/ranking.ts
@@ -0,0 +1,6 @@
+export const RANKING_TABLE_COLUMNS = ['순위', '레벨', '점수', '성공 시간'] as const;
+
+export const RANKING_MESSAGES = {
+  CONFIRM_CLEAR: '랭킹 기록을 모두 초기화할까요?',
+  EMPTY_RECORD: '아직 기록이 없습니다.',
+} as const;
