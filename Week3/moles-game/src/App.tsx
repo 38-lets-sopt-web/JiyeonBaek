3223eb34579a64a1968851344903149922ad8ffd
jyeon03 <bjlyeon2003@naver.com>
2026-05-01 17:46:21 +0900

    feat: 전체 페이지 연결

--- Week3/moles-game/src/App.tsx
+++ Week3/moles-game/src/App.tsx
@@ -1,10 +1,20 @@
-import { cn } from '@/utils/cn';
+import { useState } from 'react';
+import AppHeader from './components/layout/AppHeader';
+import type { GameMode } from './types/app';
+import GamePage from './pages/GamePage/GamePage';
+import RankingPage from './pages/RankingPage/RankingPage';
+import useRanking from './hooks/useRanking';
 
 function App() {
+  const [mode, setMode] = useState<GameMode>('game');
+  const { records, addRecord, clearRecords } = useRanking();
+
   return (
-    <div className="App">
-      <h1 className={cn('text-3xl font-bold', 'bg-amber-50 px-10')}>Moles Game</h1>
-      <p className={cn('text-4xl')}>Click on the moles to score points!</p>
+    <div className="flex h-screen flex-col gap-6 overflow-hidden px-20 py-15">
+      <AppHeader mode={mode} setMode={setMode} />
+
+      {mode === 'game' && <GamePage onSuccessGame={addRecord} />}
+      {mode === 'ranking' && <RankingPage records={records} onClear={clearRecords} />}
     </div>
   );
 }
