2ebdf1c97212d1cfebc5087961202065fc12cfaf
jyeon03 <bjlyeon2003@naver.com>
2026-05-07 03:18:31 +0900

    feat: Button 공통 컴포넌트 구현

---
+++ Week4/Homework/src/shared/ui/Button/Button.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/shared/utils/cn';
+import type { ComponentProps } from 'react';
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+const Button = ({ children, className, type = 'button', ...props }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={cn(
+        'w-full rounded-sm py-3',
+        'text-text hover:text-primary100 text-center text-xl font-medium',
+        'bg-primary200 hover:bg-primary300',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
