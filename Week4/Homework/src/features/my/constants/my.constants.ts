c16ab9534bbbe042ca4efe669732daee4ea3086d
jyeon03 <bjlyeon2003@naver.com>
2026-05-08 15:12:22 +0900

    feat: 마이페이지 상수 & 유틸함수

---
+++ Week4/Homework/src/features/my/constants/my.constants.ts
@@ -0,0 +1,16 @@
+export const MY_INFO = {
+  id: 'assignment',
+  part: '웹',
+  name: '웨비들아하이',
+  email: 'sopt@sopt.org',
+  age: '25',
+};
+
+export const MY_ERROR_MESSAGE = {
+  REQUIRED_FIELD: '모든 항목을 입력해주세요.',
+  EMAIL_FORMAT: '이메일 형식이 올바르지 않습니다.',
+  UPDATE_FAILED: '정보 수정에 실패했습니다. 입력값을 다시 확인해주세요.',
+  UPDATE_SUCCESS: '정보가 수정되었습니다.',
+};
+
+export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
