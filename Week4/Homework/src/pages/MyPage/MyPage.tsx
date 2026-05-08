c7624ae4f8c5e7bb68ad6fcbdd048b6f4b4b59ed
jyeon03 <bjlyeon2003@naver.com>
2026-05-08 15:13:29 +0900

    feat: 마이페이지의 내 정보 퍼블리싱

--- Week4/Homework/src/pages/MyPage/MyPage.tsx
+++ Week4/Homework/src/pages/MyPage/MyPage.tsx
@@ -1,5 +1,21 @@
+import Header from '@/features/my/components/Header';
+import SearchForm from '@/features/my/components/SearchForm';
+
 const MyPage = () => {
-  return <>마이페이지</>;
+  return (
+    <>
+      <Header
+        userName={localStorage.getItem('userName') ?? '웨비들아따라해님'}
+        handleLogout={() => {
+          localStorage.removeItem('userId');
+        }}
+      />
+      <div className="flex min-h-screen flex-col items-center justify-center gap-10">
+        <h1 className="text-2xl font-bold">내 정보</h1>
+        <SearchForm />
+      </div>
+    </>
+  );
 };
 
 export default MyPage;
